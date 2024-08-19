e0b3528d53ffe157ca08d878f5d4e351306abafe
gabrielhipolitoo <c.gabrielhipolitoh@gmail.com>
2024-08-18 22:02:20 -0300

    Revert "feat: create drawer"
    This reverts commit 841ca8fc2b26abccc07ba062ea1329767f717497.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+<<<<<<< HEAD
 import { RouterProvider } from "react-router-dom";
 import { AppRoutes } from "./routes/index.tsx";
 import { AppThemeProvider } from "./shared/contexts";
@@ -10,6 +11,19 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppThemeProvider>
       <RouterProvider router={router} />
+=======
+import { BrowserRouter,  } from "react-router-dom";
+import { AppRoutes } from "./routes/index.tsx";
+import { AppThemeProvider } from "./shared/contexts";
+
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppThemeProvider
+    ><BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+>>>>>>> parent of 841ca8f (feat: create drawer)
     </AppThemeProvider>
   </StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@mui/material";
+<<<<<<< HEAD
 import { createBrowserRouter, Navigate } from "react-router-dom";
+=======
+import {  Navigate, Routes,Route } from "react-router-dom";
+>>>>>>> parent of 841ca8f (feat: create drawer)
 import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
   const {toggleTheme} = useAppThemeContext()
+<<<<<<< HEAD
   const router = createBrowserRouter([
     {
       path: "/pagina-inicial",
@@ -22,4 +27,14 @@ export const AppRoutes = () => {
   return {
     router,
   };
+=======
+
+  return <Routes>
+      <Route path="/" element={<Button  onClick={toggleTheme} variant="contained" color="primary">
+          Toggle
+        </Button>} />
+
+        <Route path="*" element={<Navigate to="pagina-inicial" />} />
+  </Routes>;
+>>>>>>> parent of 841ca8f (feat: create drawer)
 };
